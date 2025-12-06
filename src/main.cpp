a216f5078455f3aab43b90f20a052f1e88c36be3
Darius Brown <dariustb@tech-center.com>
2025-12-06 01:37:34 -0600

    Removed example values

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,8 @@
 #include <opencv2/opencv.hpp>
-#include <string>
 #include <iostream>
 
 #include <vfm_imagesearch.h>
 #include <vfm_args.h>
-
-namespace {
-	const std::string video_path = "example.mp4";
-	const std::string image_path = "example_frame.jpg";
-}
 
 int main(int argc, char** argv) {
     const Args args = parseArgs(argc, argv);
@@ -22,7 +16,7 @@ int main(int argc, char** argv) {
     switch (matchStatus) {
         case ImageSearch::e_SUCCESS:
             ImSearch.exportResultFrame();
-            std::cout << "Selected frame # "    << ImSearch.result_frame_count()
+            std::cout << "Selected frame # "  << ImSearch.result_frame_count()
                     << " with confidence of " << ImSearch.result_confidence() * 100
                     << "%." << std::endl;
             break;
