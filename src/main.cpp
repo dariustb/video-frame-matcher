84c50227c49a39d4be445a6e82b7243052bfa641
Darius Brown <dariustb@tech-center.com>
2025-12-12 12:32:04 -0600

    Added specific exit code values for errored MatchStatuses

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,28 @@
 #include <iostream>
 
-#include <vfm_imagesearch.h>
 #include <vfm_args.h>
+#include <vfm_imagesearch.h>
 #include <vfm_io.h>
+#include <vfm_types.h>
 
 int main(int argc, char** argv) {
     const Args args = parseArgs(argc, argv);
-
     const MatchResults results = ImageSearch::searchVideoForImage(args.image_path, args.video_path, args.threshold);
 
     switch (results.status) {
         case MatchStatus::e_SUCCESS:
         case MatchStatus::e_NO_MATCH_FOUND:
+            printResults(results);
             break;
 
         case MatchStatus::e_BAD_FILE:
             std::cerr << "Error: could not open file" << std::endl;
-            break;
+            return 2;
 
         default:
             std::cerr << "Error: unexpected return value" << std::endl;
-            break;
-    
+            return 3;
     }
-
-    printResults(results);
 
 	return 0;
 }
