3477b17a0f2e6d515b609fa7aee3cbcf53401ce4
Darius Brown <dariustb@tech-center.com>
2025-12-12 12:31:18 -0600

    Cleaned up printResults

--- src/io/vfm_io.cpp
+++ src/io/vfm_io.cpp
@@ -6,20 +6,20 @@
 
 void printResults(const MatchResults &results)
 {
-    if (results.status == MatchStatus::e_SUCCESS) {
-        // Find the best match
-        auto best_match = std::max_element(results.matches.begin(), results.matches.end(),
-        [](const Match& a, const Match& b) { return a.score < b.score; });
-
-        if (best_match != results.matches.end()) {
-        std::cout << "Found " << results.matches.size() << " match(es)" << std::endl;
-        std::cout << "Best match at frame # " << best_match->frame_index
-                << " (" << best_match->time_seconds << "s)"
-                << " with confidence of " << best_match->score * 100
-                << "%." << std::endl;
-        }
-    }
-    else if (results.status == MatchStatus::e_NO_MATCH_FOUND) {
+    if (results.status == MatchStatus::e_NO_MATCH_FOUND) {
         std::cout << "No match found" << std::endl;
+        return;
     }
+
+    // Find the best match
+    auto best_match = std::max_element(results.matches.begin(), results.matches.end(),
+    [](const Match& a, const Match& b) { return a.score < b.score; });
+
+    if (best_match != results.matches.end()) {
+    std::cout << "Found " << results.matches.size() << " match(es)" << std::endl;
+    std::cout << "Best match at frame # " << best_match->frame_index
+            << " (" << best_match->time_seconds << "s)"
+            << " with confidence of " << best_match->score * 100
+            << "%." << std::endl;
+    }   
 }
